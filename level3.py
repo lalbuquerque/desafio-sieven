5cc49321a7ed3f5e2e7b399c19dbed9bfce0ed16
loliveira <lucas.oliveira@acotel.com>
2013-11-08 18:17:04 -0200

    Retirando instanciação desnecessaria da classe RequestUrl

--- level3.py
+++ level3.py
@@ -9,7 +9,6 @@ request = request.RequestUrl()
 site_status_1 = request.open_url_and_get_content('http://hughes.sieve.com.br:8000/level3/', headers=headers)
 headers = { 'User-Agent' :  'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:25.0) Gecko/20100101 Firefox/25.0',
             'Cookie' :  'd53db4de415c4e858dc761595623a898=+; Path=/\r\n'  }
-request = request.RequestUrl()
 site_status_1 = request.open_url_and_get_content('http://hughes.sieve.com.br:8000/level3/', headers=headers)
 pattern = r'href="(.*?)"'
 match = re.search(pattern, site_status_1)
