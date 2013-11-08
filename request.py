5346235ec8b2f1dbb2175ab84eb9f5b944f98f57
loliveira <lucas.oliveira@acotel.com>
2013-11-08 18:02:49 -0200

    Retirando comentario desnecessario

--- request.py
+++ request.py
@@ -4,7 +4,6 @@ import urllib2
 class RequestUrl:
 
     def open_url_and_get_content(self, url, data=None, headers={}):
-        #headers = urllib.urlencode(params)
         request = urllib2.Request(url, data, headers)
         response = urllib2.urlopen(request)
         feeddata = response.read()
