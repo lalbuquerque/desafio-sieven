dc8e4352272231bf25fcf126c46e450fdd18008f
loliveira <lucas.oliveira@acotel.com>
2013-11-11 16:13:40 -0200

    Alterando a forma como são passados os cookies

--- level4.py
+++ level4.py
@@ -5,10 +5,9 @@ import request
 
 request = request.RequestUrl()
 headers = { 'User-Agent' :  'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:25.0) Gecko/20100101 Firefox/25.0',
-            'Cookie' :  'd53db4de415c4e858dc761595623a898=+; Path=/\r\n',
-            'Cookie' :  '18=+; Path=/\r\n',
-            'Cookie' : 'cade-meu-cookie=esta+aqui; Path=/\r\n' }       
-feeddata = request.open_url_and_get_content('http://hughes.sieve.com.br:8000/level4/')
+            'Cookie'     :  'd53db4de415c4e858dc761595623a898=+; 18=+; cade-meu-cookie=esta+aqui;'
+            }
+feeddata = request.open_url_and_get_content('http://hughes.sieve.com.br:8000/level4/', headers=headers)
 locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 pattern = r'R\$ ([\d\.,]+)'
 match = re.search(pattern, feeddata)
