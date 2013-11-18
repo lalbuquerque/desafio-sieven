5cf504d12a22478b50e69443eb2befd25f7fb9c5
loliveira <lucas.oliveira@acotel.com>
2013-11-18 15:35:44 -0200

    Realizando um refactoring na classe Request para que esteja preparada para a fase 5

--- request.py
+++ request.py
@@ -3,8 +3,14 @@ import urllib2
 
 class RequestUrl:
 
-    def open_url_and_get_content(self, url, data=None, headers={}):
+    def open_url_and_get_content(self, url, data=None, headers={}, replaces={}):
         request = urllib2.Request(url, data, headers)
         response = urllib2.urlopen(request)
         feeddata = response.read()
+        if replaces is not None:
+            for k, v in replaces.items():
+                url = response.geturl().replace(k, v)
+                request = urllib2.Request(url, data, headers)
+                response = urllib2.urlopen(request)
+                feeddata = response.read()
         return feeddata
